bc422bc917ecc2d3207353661a2968cb3d97a56b
Justin Duong <jduong@umich.edu>
2022-08-15 21:04:07 -0400

    Did work on credit card number patter.

--- js/form_active_states.js
+++ js/form_active_states.js
@@ -1,7 +1,3 @@
-function validate(input) {
-  
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   const inputDivs = document.querySelectorAll('label div');
   const labelCardNumber = document.getElementById('label-number');
@@ -22,11 +18,19 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   labelCardNumber.addEventListener('input', (e) => {
-    const numberArray = e.target.value.split('');
+    let numberArray;
+    if (e.target.value.length > 4) {
+      numberArray = e.target.value.split(' ');
+    } else {
+      numberArray = e.target.value.split('');
+    }
+
+    console.log(numberArray);
     // Going through numbers typed in input box.
     // if character cannot be parsed as a number, then we want to add an error.
     for (let i = 0; i < numberArray.length; i++) {
-      if (!parseInt(numberArray[i])) {
+      if (isNaN(numberArray[i])) {
+        console.log('error');
         labelCardNumber.children[0].classList.add('error');
         labelCardNumber.children[1].classList.remove('hide');
       } else {
@@ -36,5 +40,29 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
     }
+
+    if (e.target.value.length > 4) {
+      numberArray = numberArray.join('');
+      console.log(numberArray);
+    }
+
+    // Add a space after 4 digits.
+
+    if (numberArray.length % 4 === 0 && e.target.value.length < 19) {
+      console.log('need to add space');
+      e.target.value = `${e.target.value} `;
+    }
+  });
+
+  const submitBtn = document.getElementById('form-submit-btn');
+
+  submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const cardNumberInput = labelCardNumber.children[0].children[0].value;
+    console.log((cardNumberInput).toString());
+    const regEx = new RegExp('^4[0-9]{12}(?:[0-9]{3})?$');
+
+    console.log(regEx.test(cardNumberInput));
   });
 });
